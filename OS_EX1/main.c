841060d80efb6fa6a548cc313b840d48a2497ea5
Maor <gmaor88@gmail.com>
2017-11-02 18:01:25 +0200

    add_polygon() done and checked + added masks and cosmetics

--- OS_EX1/main.c
+++ OS_EX1/main.c
@@ -1,15 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+/**************************************************/
+///					Include						///
+/**************************************************/
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+/***************************************************/
 
-#define NUM_OF_VERTICES_IN_TRIANGLE 3
-#define NUM_OF_VERTICES_IN_QUAD 4
-#define INPUT_STRING_LEN 16
-#define True 1
-#define False 0
-#define NEW_POLIGON_MASK 0x2
+/**************************************************/
+///					Define						///
+/**************************************************/
+#define True									1
+#define False									0
+#define NUM_OF_VERTICES_IN_TRIANGLE				3
+#define NUM_OF_VERTICES_IN_QUAD					4
+#define INPUT_STRING_LEN						16
 
+/***************************************************/
+
+/**************************************************/
+///				MASKS And Shift					///
+/**************************************************/
+#define NEW_POLIGON_MASK						0x2
+#define POLYGON_TYPE_MASK						0x4
+#define ACTIONS_MASK							0x38
+#define POLYGONS_TO_DO_ACTIONS_ON_MASK			0xc0   
+#define COORDINATES_MASK						0xFFF
+#define COORDINATES_VALUE_MASK					0x3f
+#define	SHIFT_TO_FIRST_VERTEX					8
+#define SHIFT_TO_SECOND_VERTEX					20
+#define SHIFT_TO_THIRD_VERTEX					32
+#define SHIFT_TO_FOURTH_VERTEX					44
+#define SHIFT_TO_Y_VALUE						6
+
+/***************************************************/
+
+/**************************************************/
+///				Typedef And Structs				///
+/**************************************************/
 typedef struct _vertex {
 	float x, y;
 } Vertex;
@@ -32,10 +61,16 @@ typedef struct list {
 	ListNode* tail;
 }List;
 
+typedef void(*func_ptr)(long long unsigned);
 
+/***************************************************/
+
+/**************************************************/
+///					Prototyps					///
+/**************************************************/
 void analyze_and_exec(long long unsigned poligon);
 long long unsigned get_poligon_from_user();
-void populate_function_array();
+void populate_functions_array();
 void init_poligon_list();
 int is_end_of_input(long long unsigned poligon);
 void free_poligon_list(ListNode* currentNode);
@@ -48,14 +83,23 @@ void print_polygon(long long unsigned poligon);
 void do_current(long long unsigned poligon);
 void do_all(long long unsigned poligon);
 
+/***************************************************/
 
-void(*function_array[6]);
+/**************************************************/
+///						Globals					///
+/**************************************************/
+func_ptr functions_array[6];
 List* poligon_list;
 
+/***************************************************/
+
+/**************************************************/
+///						Main					///
+/**************************************************/
 void main() {
 	long long unsigned poligon = 0;
 
-	populate_function_array();
+	populate_functions_array();
 	init_poligon_list();
 
 	while (True)
@@ -69,11 +113,15 @@ void main() {
 
 	free_poligon_list(poligon_list->head);
 }
+/***************************************************/
 
 
+/**************************************************/
+///				Implementations					///
+/**************************************************/
 void analyze_and_exec(long long unsigned poligon) {
 	if (poligon & NEW_POLIGON_MASK) {
-		add_polygon(poligon);
+		functions_array[0](poligon);
 	}
 
 
@@ -87,13 +135,13 @@ long long unsigned get_poligon_from_user() {
 	return poligon;
 }
 
-void populate_function_array() {
-	function_array[0] = add_polygon;
-	function_array[1] = perimeter;
-	function_array[2] = area;
-	function_array[3] = print_polygon;
-	function_array[4] = do_current;
-	function_array[5] = do_all;
+void populate_functions_array() {
+	functions_array[0] = add_polygon;
+	functions_array[1] = perimeter;
+	functions_array[2] = area;
+	functions_array[3] = print_polygon;
+	functions_array[4] = do_current;
+	functions_array[5] = do_all;
 }
 
 void init_poligon_list() {
@@ -124,9 +172,17 @@ void free_poligon_list(ListNode* currentNode) {
 }
 
 
+
+
 /* add new polygon to the list*/
 void add_polygon(long long unsigned poligon) {
-
+	ListNode *node = (ListNode*)malloc(sizeof(ListNode));
+	//init mode
+	node->polygon = poligon;
+	node->next = NULL;
+	//add to list and set the list new tail
+	poligon_list->tail->next = node;
+	poligon_list->tail = node;
 }
 
 /* calculate and print the perimeter */
@@ -153,3 +209,5 @@ void do_current(long long unsigned poligon) {
 void do_all(long long unsigned poligon) {
 
 }
+
+/***************************************************/
